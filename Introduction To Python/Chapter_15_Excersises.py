602c25f301e26db8d528612be56378b82c64b083
Equinox <info@importantpeople.nl>
2023-09-06 00:17:38 +0200

    Chapter 15 completed and added some mild python criticism. I will never love concurrency in Python.

--- Introduction To Python/Chapter_15_Excersises.py
+++ Introduction To Python/Chapter_15_Excersises.py
@@ -1,1 +1,30 @@
 # Things to do, chapter 15
+import multiprocessing
+import datetime
+import time
+from random import random
+# from CustomImports import huh
+
+
+# Because concurrency absolutely sucks in python, we have to move anything we don't want to run on EVERY process to
+# the segment under if __name__ == "__main__":
+# If you think that's weird, importing another file with top-level executions will do the same.
+# Are you confused? Just uncomment the disabled import statement at the top of this file to see this in action
+
+def wait_a_little():
+    # Random.random() returns a float beteen 0 and 1
+    time.sleep(random())
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+
+    # 15.1 Use multiprocessing to create three separate processes. Make each one wait a random number of seconds between
+    # zero and one, print the current time, and then exit.
+    print("\n------ 15.1 ------")
+
+    for n in range(4):
+        p = multiprocessing.Process(target=wait_a_little)
+        p.daemon = True
+        p.start()
+        p.join()
---
+++ Introduction To Python/CustomImports/huh.py
@@ -0,0 +1,2 @@
+# Yes, Python executes top level function calls
+print("Wait, I only imported the file. Why is it running?!?")
